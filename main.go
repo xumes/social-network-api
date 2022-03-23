9df7e55d67e9589ffaf2c9fd65d5209a424a31c7
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-03-23 16:54:14 -0300

    feat: create the server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Running the API")
+
+	appRouter := router.Generate()
+
+	log.Fatal(http.ListenAndServe(":5080", appRouter))
 }
