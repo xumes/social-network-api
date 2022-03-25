612e0e3f84d75d3848d6f748c1e5b6027be3e358
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-03-25 11:33:29 -0300

    feat: add database connection method

--- main.go
+++ main.go
@@ -10,11 +10,9 @@ import (
 
 func main() {
 	config.Load()
-	fmt.Printf("Running the API at port: %d\n", config.Port)
-
-	fmt.Printf("DB Connection string: %s\n", config.DatabaseConnectionString)
 
 	appRouter := router.Generate()
 
+	fmt.Printf("Listen API at port: %d\n", config.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), appRouter))
 }
