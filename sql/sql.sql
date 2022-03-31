808919b84e57e583bb3ec8904f33704a6d8cdbba
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-03-31 16:42:12 -0300

    chore: create posts schema

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
     id int auto_increment primary key,
@@ -26,3 +27,15 @@ CREATE TABLE followers(
 
     PRIMARY KEY(user_id, follower_id)
 )ENGINE=INNODB;
+
+CREATE TABLE posts(
+    id int auto_increment primary key,
+    title varchar(50) not null,
+    content varchar(300) not null,
+    author_id int not null,
+    FOREIGN KEY (author_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+    likes int default 0,
+    created_at timestamp default current_timestamp()
+) ENGINE=INNODB;
