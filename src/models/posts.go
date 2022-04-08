e387e0487184a65a60f9cdcd173e698f918e0ee1
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-04-08 09:21:44 -0300

    feat: add Posts struct

---
+++ src/models/posts.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Posts struct {
+	Id         uint64    `json:"id,omitempty"`
+	Title      string    `json:"title,omitempty"`
+	Content    string    `json:"content,omitempty"`
+	AuthorId   uint64    `json:"authorId,omitempty"`
+	AuthorNick uint64    `json:"authorNick,omitempty"`
+	Likes      uint64    `json:"likes"`
+	CreatedAt  time.Time `json:"createdAt,omitempty"`
+}
